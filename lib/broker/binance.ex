e0f99c659a2c993c0db1b8d6418d74c9dd246b56
Rob <rm.rf.etc@gmail.com>
2020-11-10 18:40:08 -0800

    Fix binance subscription channel type string

--- lib/broker/binance.ex
+++ lib/broker/binance.ex
@@ -2,29 +2,32 @@ defmodule MarketClient.Broker.Binance do
   alias MarketClient.Resource
   alias Jason, as: J
 
-  def url(%Resource{asset_id: id}) do
-    "wss://stream.binance.com:9443/ws/#{get_pair(id)}"
+  def url(res = %Resource{}) do
+    "wss://stream.binance.com:9443/ws/#{get_pair(res)}"
   end
 
-  def get_pair({:crypto, {c1, c2}}) when is_atom(c1) and is_atom(c2),
-    do: "#{to_string(c1)}#{to_string(c2)}"
+  def get_pair(%Resource{asset_id: {:crypto, {c1, c2}}}) when is_atom(c1) and is_atom(c2) do
+    "#{to_string(c1)}#{to_string(c2)}"
+  end
 
-  def format_asset_id(id), do: "#{get_pair(id)}@trade"
+  def format_asset_id(res = %Resource{}) do
+    "#{get_pair(res)}@bookTicker"
+  end
 
-  def subscribe(%Resource{asset_id: id}) do
+  def subscribe(res = %Resource{}) do
     %{
       "id" => 1,
       "method" => "SUBSCRIBE",
-      "params" => [format_asset_id(id)]
+      "params" => [format_asset_id(res)]
     }
     |> J.encode!()
   end
 
-  def unsubscribe(%Resource{asset_id: id}) do
+  def unsubscribe(res = %Resource{}) do
     %{
       "id" => 1,
       "method" => "UNSUBSCRIBE",
-      "params" => [format_asset_id(id)]
+      "params" => [format_asset_id(res)]
     }
     |> J.encode!()
   end
